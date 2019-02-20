2ba49d492ac079b9eff0f78345bcc2d056db7343
Tomasz Knapik <hi@tmkn.org>
2019-02-20 18:24:38 +0000

    Update

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "-*-terminus-medium-r-normal-*-14-*-*-*-*-*-*-*", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Noto Sans Mono:size=10", "Monospace:size=10" };
+static const char dmenufont[]       = "Noto Sans Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,8 +63,8 @@ static const char terminal[] = "urxvtc";
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *kill_xinit[] = { "killall", "xinit", NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *clipmenucmd[] = { "clipmenu", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *clipmenucmd[] = { "clipmenu", "-i", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *clipmenudelcmd[] = { "clipdel", "-d", ".*", NULL };
 static const char *termcmd[]  = { terminal, "-e", "tmux", "new-session", NULL };
 static const char *alt_termcmd[]  = { terminal, NULL };
@@ -73,7 +73,7 @@ static const char *file_manager[] = { terminal, "-e", "tmux", "new-session", "ra
 static const char *web_browser[] = { "xdg-open", "https://", NULL };
 static const char *password_manager[] = {"keepassxc", NULL };
 
-static const char *dpms_suspend[] = { "xset", "dpms", "force", "suspend", NULL };
+static const char lock_command[] = "xset dpms force suspend; slock";
 
 static const char *switch_monitor[] = { "mons", "-n", "right", NULL };
 
@@ -144,8 +144,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = kill_xinit } },
 
     // General
-	{ MODKEY|ShiftMask,             XK_l,      spawn,         {.v = dpms_suspend} },
-	{ 0,                            XF86XK_ScreenSaver, spawn, {.v = dpms_suspend} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,         SHCMD(lock_command) },
+	{ 0,                            XF86XK_ScreenSaver, spawn, SHCMD(lock_command) },
 	{ 0,                            XF86XK_Tools, spawn,      {.v = volume_settings } },
 	{ 0,                            XF86XK_Display, spawn,    {.v = switch_monitor } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,         {.v = web_browser} },
@@ -170,8 +170,8 @@ static Key keys[] = {
     // Music
     { MODKEY|ShiftMask,              XK_m, spawn, {.v = music_player } },
     { 0,                             XF86XK_AudioPlay, spawn, {.v = music_play_pause } },
-    { 0,                             XF86XK_LaunchA, spawn, {.v = music_play_pause } },
-    { Mod4Mask,                      XK_apostrophe, spawn, {.v = music_next } },
+    { Mod4Mask,                      XK_apostrophe, spawn, {.v = music_play_pause } },
+    { Mod4Mask,                      XK_numbersign, spawn, {.v = music_next } },
     { 0,                             XF86XK_AudioNext, spawn, {.v = music_next } },
     { Mod4Mask,                      XK_semicolon, spawn, {.v = music_prev } },
     { 0,                             XF86XK_AudioPrev, spawn, {.v = music_prev } },
